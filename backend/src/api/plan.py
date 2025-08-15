6a2426f853b93e7eddf73c91f0e69e63fd60f331
McArthur-Alford <mcarthur.alford@proton.me>
2025-08-15 22:48:59 +0000

    Plans

--- backend/src/api/plan.py
+++ backend/src/api/plan.py
@@ -1,1 +1,11 @@
-"""Course Plan."""
+"""A Complete User Plan."""
+
+from uuid import UUID
+
+
+class Plan:
+    name: str
+    plan_id: UUID
+    auth: UUID
+    # maps (year, sem) to list of chosen courses
+    courses: dict[tuple[int, int], list[str]]
