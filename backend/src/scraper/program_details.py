857dc92115cd412b9d7aebfc5a97ef968dc77b05
Dongyug David Jeong <donghyug.jeong@student.uq.edu.au>
2025-08-16 00:48:41 +1000

    Add program detail scraper

---
+++ backend/src/scraper/program_details.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+import cloudscraper
+import json
+
+DETAILS_URL = "https://programs-courses.uq.edu.au/requirements/program/{}/{}"
+
+
+def extract_details(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+    # Find the <div id="main-content">
+    main_content = soup.find("div", id="main-content")
+    if not main_content:
+        return None
+
+    # Find the first <script type="text/javascript"> inside main-content
+    script_tag = main_content.find("script", type="text/javascript")
+    if not script_tag:
+        return None
+
+    # Get the script's content
+    script_content = script_tag.string
+    return script_content
+
+
+def fetch_programs():
+    with open("program.json", "r") as f:
+        data = json.load(f)
+    return list(set([item["program_id"] for item in data]))
+
+
+def fetch_page(program_id: int, year: int) -> list:
+    scraper = cloudscraper.create_scraper()    
+    try:
+        url = DETAILS_URL.format(program_id, year)
+        print(url)
+        response = scraper.get(url)
+        response.raise_for_status()
+        return extract_details(response.text)
+    except Exception as e:
+        print(f"Error fetching page {url}: {e}")
+        return []
+
+
+def scrape_all_program_details(programs: list[str]) -> list:
+    all_programs = []
+    for program_id in programs:
+        year_data = {}
+        for year in range(2021, 2027):
+            details = fetch_page(program_id, year)
+            year_data[str(year)] = details
+        all_programs.append({
+            "program_id": program_id,
+            "data": year_data
+        })
+    return all_programs
+
+
+if __name__ == "__main__":
+    programs = fetch_programs()
+    details = scrape_all_program_details(programs)
+    with open("program_details.json", "w") as f:
+        json.dump(details, f, indent=2)
