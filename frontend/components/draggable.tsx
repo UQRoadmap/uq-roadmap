d246e226f94d530c6d1d29df5cd2a899e269fbd1
JTrenerry <105094182+JTrenerry@users.noreply.github.com>
2025-08-16 11:04:47 +1000

    linting and types

--- frontend/components/draggable.tsx
+++ frontend/components/draggable.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import {useDraggable} from '@dnd-kit/core';
+import type { ReactNode } from 'react';
+import { Course } from '@/types/course'
 
-export default function Draggable(props) {
+type DraggableProps = {
+  id: string;
+  children: ReactNode;
+  data: Course; // or `Course` if you always pass course objects
+};
+
+export default function Draggable(props:DraggableProps) {
   const {attributes, listeners, setNodeRef} = useDraggable({
     id: props.id,
     data: props.data,
